20e09500918694ac84c337968d46c385775390c0
98kevinxiao <101177018+98kevinxiao@users.noreply.github.com>
2022-06-17 10:50:46 +0800

    Create icon.js

---
+++ icon.js
@@ -0,0 +1,19 @@
+	{
+"name":"Position figure", 
+"description":"Designed by universal netizen.", 
+"icons":
+[
+
+{"name":"ㅤㅤㅤ1", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs1.png"},     
+{"name":"ㅤㅤㅤ2", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs2.png"},
+{"name":"ㅤㅤㅤ3", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs3.png"},
+{"name":"ㅤㅤㅤ4", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs4.png"},
+{"name":"ㅤㅤㅤ5", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs5.png"},
+{"name":"ㅤㅤ  6", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs6.png"},
+{"name":"ㅤㅤㅤ7", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs7.png"},
+{"name":"ㅤㅤㅤ8", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs8.png"},
+{"name":"ㅤㅤㅤ9", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs9.png"},
+{"name":"ㅤㅤㅤ10", "url":"https://raw.githubusercontent.com/98kevinxiao/server/main/icon/zs10.png"},
+	
+]
+}
