25a8e46c2db266129a6540149667c888458c78e9
98kevinxiao <101177018+98kevinxiao@users.noreply.github.com>
2022-07-26 13:49:12 +0800

    Update IP-API.js

--- IP-API.js
+++ IP-API.js
@@ -33,7 +33,7 @@ var flags = new Map([["AC","🇦🇨"],["AD","🇦🇩"],["AE","🇦🇪"],["AF"
 var body = $response.body;
 var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ obj['country'];
-var subtitle =City_ValidCheck(obj['city'])'+' ™ '+' ➠ '+'(obj['query'])'+'('+ ISP_ValidCheck)')'+' ™ '
+var subtitle =City_ValidCheck(obj['city'])'+' ™ '+' ➠ '+'(obj['query'])'+'('+ ISP_ValidCheck(obj['org'])+')'+' ™ '
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
