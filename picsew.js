a891a7a5286eb0bd09776cbeaddd234faeb3a5ac
98kevinxiao <101177018+98kevinxiao@users.noreply.github.com>
2022-11-21 13:17:00 +0800

    Update picsew.js

--- picsew.js
+++ picsew.js
@@ -8,7 +8,7 @@
 
 [rewrite_local]
 # > Picsew解锁专业版
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/PicsewProCrack.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/98kevinxiao/javascript/main/picsew.js
 
 [mitm] 
 hostname = buy.itunes.apple.com
